61b2feb7f991511be26006ef9857284d76c8c7cd
Sudarshan Waghmare <sudarshanwaghmare143@gmail.com>
2025-01-04 19:24:14 +0530

    This program is to create Item, dataset nad link DS to IR.

--- FaithItem.cxx
+++ FaithItem.cxx
@@ -10,13 +10,13 @@ int ITK_user_main(int 	argc, char* argv[]) {
 	tag_t userTag = NULLTAG;
 	tag_t Ufolder = NULLTAG;
 	tag_t uom = NULLTAG;
+	tag_t DStype = NULLTAG;
+	tag_t DScreate_input = NULLTAG;
+	tag_t NewDS = NULLTAG;
 
 	// variable
 	char* item_id = NULL;
 	char* item_name = NULL; 
-
-
-
 	int status = ITK_ok;
 
 	char* user = ITK_ask_cli_argument("-u=");
@@ -39,21 +39,30 @@ int ITK_user_main(int 	argc, char* argv[]) {
 	status = AOM_set_value_string(create_input, "a3MakeBuy", "Make");
 	status = TCTYPE_create_object(create_input,&NewItem);
 	status = AOM_save_with_extensions(NewItem);
+	
 
+	status = TCTYPE_ask_type("Dataset", &DStype);
+	status = TCTYPE_construct_create_input(type, &DScreate_input);
+	status = AOM_set_value_string(create_input, "object_name", "Faith Dataset");	
+	status = TCTYPE_create_object(create_input, &NewDS);
+	status = AOM_save_with_extensions(NewDS);
 
 	status = SA_find_user2(user, &userTag);
 	status = SA_ask_user_home_folder(userTag, &Ufolder);
 	status = FL_insert(Ufolder, NewItem, 999);
 	status = AOM_save_with_extensions(Ufolder);
 
-	// printing newly created item	
+
+
+
+	/* printing newly created item	
 	char* UOMvalue = NULL;
 	status = ITEM_ask_id2(NewItem, &item_id);
-	status = ITEM_ask_name2(NewItem, &item_name);
+	status = ITEM_ask_name2(0NewItem, &item_name);
 	status = ITEM_ask_unit_of_measure(NewItem, &uom);
 	
 	printf("Item ID\t\tItem Name\t\tUOM\n");
-	printf("%s\t%s\t%d", item_id, item_name, uom);
+	printf("%s\t%s\t%d", item_id, item_name, uom);*/
 
 	status = ITK_exit_module(true);
 	if (status == ITK_ok) {
