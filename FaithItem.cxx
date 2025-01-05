0ea8927c9f6c97486e8b98fda1441e505810c37f
Sudarshan Waghmare <sudarshanwaghmare143@gmail.com>
2025-01-05 15:30:04 +0530

    file handling added

--- FaithItem.cxx
+++ FaithItem.cxx
@@ -13,19 +13,16 @@ int ITK_user_main(int 	argc, char* argv[]) {
 	tag_t DScreate_input = NULLTAG;
 	tag_t NewDS = NULLTAG;
 
-
 	//Get latest revision
 	tag_t Item_rev = NULLTAG;
-
 
 	//GRM Relation creation
 	tag_t relation_type = NULLTAG;
 	tag_t GRMrelation = NULLTAG;
 
-
 	// variable
 	char* item_id = NULL;
-	char* item_name = NULL; 
+	char* item_name = NULL;
 	int status = ITK_ok;
 
 	char* user = ITK_ask_cli_argument("-u=");
@@ -40,15 +37,15 @@ int ITK_user_main(int 	argc, char* argv[]) {
 		printf("Failed to login.\n");
 	}
 
-	//Item create
-	status = TCTYPE_ask_type("A3BHMakePart", &type);		
+	/*//Item create
+	status = TCTYPE_ask_type("A3BHMakePart", &type);
 	status = TCTYPE_construct_create_input(type, &create_input);
 	status = AOM_set_value_string(create_input, "object_name", "Faith Item");
 	status = AOM_set_value_string(create_input, "uom_tag", "Each");
 	status = AOM_set_value_string(create_input, "a3MakeBuy", "Make");
-	status = TCTYPE_create_object(create_input,&NewItem);
+	status = TCTYPE_create_object(create_input, &NewItem);
 	status = AOM_save_with_extensions(NewItem);
-	
+
 	//Dataset create
 	status = TCTYPE_find_type("PDF", "Dataset", &DStype);
 	status = TCTYPE_construct_create_input(DStype, &DScreate_input);
@@ -61,7 +58,7 @@ int ITK_user_main(int 	argc, char* argv[]) {
 
 	// GRM relation
 	status = GRM_find_relation_type("IMAN_specification", &relation_type);
-	status = GRM_create_relation(Item_rev, NewDS,relation_type, NULLTAG, &GRMrelation);
+	status = GRM_create_relation(Item_rev, NewDS, relation_type, NULLTAG, &GRMrelation);
 	status = GRM_save_relation(GRMrelation);
 	status = AOM_save_with_extensions(NewItem);
 
@@ -85,9 +82,79 @@ int ITK_user_main(int 	argc, char* argv[]) {
 	status = ITEM_ask_id2(NewItem, &item_id);
 	status = ITEM_ask_name2(NewItem, &item_name);
 	status = ITEM_ask_unit_of_measure(NewItem, &uom);
-	
+
 	printf("\nItem ID\t\tItem Name\tUOM\n");
-	printf("%s\t%s\t%d\n", item_id, item_name, uom);
+	printf("%s\t%s\t%d\n", item_id, item_name, uom); */
+
+	// place item to home folder
+	status = SA_find_user2(user, &userTag);
+	status = SA_ask_user_home_folder(userTag, &Ufolder);
+
+
+	FILE* fp;
+	char fline[500];
+	char* ItemName;
+	char* MakeBuy;
+	char* DSpath;
+	char* UOM;
+	fp = fopen("G:\\My Drive\\FaithPLM\\Online Batch Recorded Sessions\\4. Customization\\ITK_Program Practice\\Batch_Utility\\20-30 KVA 300-350 C FRAME HAWKINS COOKER\\20-30 KVA 300-350 C FRAME HAWKINS COOKER BOM.csv", "a+");
+	printf("\nItem ID\t\tItem Name\tUOM\n");
+	while (fgets(fline, 500, fp)) {
+		ItemName = strtok(fline, ",");
+		MakeBuy = strtok(NULL, ",");
+		UOM = strtok(NULL, ",");
+		DSpath = strtok(NULL, ",");
+
+		//Item create
+		status = TCTYPE_ask_type("A3BHMakePart", &type);
+		status = TCTYPE_construct_create_input(type, &create_input);
+		status = AOM_set_value_string(create_input, "object_name", ItemName);
+		status = AOM_set_value_string(create_input, "uom_tag", UOM);
+		status = AOM_set_value_string(create_input, "a3MakeBuy", MakeBuy);
+		status = TCTYPE_create_object(create_input, &NewItem);
+		status = AOM_save_with_extensions(NewItem);
+
+		//Dataset create
+		status = TCTYPE_find_type("PDF", "Dataset", &DStype);
+		status = TCTYPE_construct_create_input(DStype, &DScreate_input);
+		status = AOM_set_value_string(DScreate_input, "object_name", ItemName);
+		status = TCTYPE_create_object(DScreate_input, &NewDS);
+		status = AOM_save_with_extensions(NewDS);
+
+		//get latest Rev
+		status = ITEM_ask_latest_rev(NewItem, &Item_rev);
+
+		// GRM relation
+		status = GRM_find_relation_type("IMAN_specification", &relation_type);
+		status = GRM_create_relation(Item_rev, NewDS, relation_type, NULLTAG, &GRMrelation);
+		status = GRM_save_relation(GRMrelation);
+		status = AOM_save_with_extensions(NewItem);
+
+		//import name reference
+
+		//status = AOM_lock(NewDS);
+		status = AOM_refresh(NewDS, TRUE); //CHECKOUT THE DATASET
+		status = AE_import_named_ref(NewDS, "PDF_Reference", DSpath, "", SS_BINARY); // giving double backward slash in must(\\), if not given file will not import in dataset
+		status = AOM_save_without_extensions(NewDS);
+		status = AOM_refresh(NewDS, FALSE); // CHECKIN THE DATASET
+
+		// place item to home folder
+		status = FL_insert(Ufolder, NewItem, 999);
+		status = AOM_save_with_extensions(Ufolder);
+
+
+		// printing newly created item	
+		char* UOMvalue = NULL;
+		status = ITEM_ask_id2(NewItem, &item_id);
+		status = ITEM_ask_name2(NewItem, &item_name);
+		status = ITEM_ask_unit_of_measure(NewItem, &uom);
+		
+		printf("%s\t%s\t%d\n", item_id, item_name, uom);
+	}
+
+
+
+
 
 	status = ITK_exit_module(true);
 	if (status == ITK_ok) {
@@ -97,6 +164,9 @@ int ITK_user_main(int 	argc, char* argv[]) {
 		printf("\nFailed to logout.\n");
 	}
 
+
+	fclose(fp);
+	printf("Utlity completed.\n");
 	(void)_getch();
 	return status;
 }
