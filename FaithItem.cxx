049089d451b245a467ea63fc3fe803400a7433b2
Sudarshan Waghmare <sudarshanwaghmare143@gmail.com>
2025-01-09 20:25:40 +0530

    New docx dataset added to code

--- FaithItem.cxx
+++ FaithItem.cxx
@@ -16,9 +16,19 @@ int ITK_user_main(int 	argc, char* argv[]) {
 	tag_t Item_rev = NULLTAG;
 	tag_t Huser_folder = NULLTAG;
 
-	//GRM Relation creation
+	// docx tags 
+	tag_t DocxDStype = NULLTAG;
+	tag_t DocxDScreate_input = NULLTAG;
+	tag_t DocxNewDS = NULLTAG;
+
+	//GRM Relation creation--PDF
 	tag_t relation_type = NULLTAG;
 	tag_t GRMrelation = NULLTAG;
+
+
+	// GRM relation creation--docx
+	tag_t Docxrelation_type = NULLTAG;
+	tag_t GRMrelationDocx = NULLTAG;
 
 	// variable
 	char* item_id = NULL;
@@ -39,7 +49,7 @@ int ITK_user_main(int 	argc, char* argv[]) {
 
 	status = TCTYPE_find_type("Folder", "Folder", &type);
 	status = TCTYPE_construct_create_input(type, &create_input);
-	status = AOM_set_value_string(create_input, "object_name", "BulkItem_Dieset Assy");
+	status = AOM_set_value_string(create_input, "object_name", "Hawkins C frame");
 	status = TCTYPE_create_object(create_input, &NewFolder);
 	status = AOM_save_with_extensions(NewFolder);
 
@@ -57,6 +67,9 @@ int ITK_user_main(int 	argc, char* argv[]) {
 	char* DSpath;
 	char* UOM;
 	char* owner;
+	char* DocxDSPath;
+
+
 	fp = fopen("G:\\My Drive\\FaithPLM\\Online Batch Recorded Sessions\\4. Customization\\ITK_Program Practice\\Batch_Utility\\20-30 KVA 300-350 C FRAME HAWKINS COOKER\\20-30 KVA 300-350 C FRAME HAWKINS COOKER BOM.csv", "a+");
 	printf("\nItem ID\t\tItem Name\tUOM\n");
 	while (fgets(fline, 500, fp)) {
@@ -64,7 +77,8 @@ int ITK_user_main(int 	argc, char* argv[]) {
 		MakeBuy = strtok(NULL, ",");
 		UOM = strtok(NULL, ",");
 		DSpath = strtok(NULL, ",");
-		owner = strtok(NULL, ",");
+		DocxDSPath = strtok(NULL, ",");
+		owner = strtok(NULL, "\N");
 
 		//Item create
 		status = TCTYPE_ask_type("A3BHMakePart", &type);
@@ -75,30 +89,47 @@ int ITK_user_main(int 	argc, char* argv[]) {
 		status = TCTYPE_create_object(create_input, &NewItem);
 		status = AOM_save_with_extensions(NewItem);
 
-		//Dataset create
+		//get latest Rev
+		status = ITEM_ask_latest_rev(NewItem, &Item_rev);
+
+		//Dataset create PDF
 		status = TCTYPE_find_type("PDF", "Dataset", &DStype);
 		status = TCTYPE_construct_create_input(DStype, &DScreate_input);
 		status = AOM_set_value_string(DScreate_input, "object_name", ItemName);
 		status = TCTYPE_create_object(DScreate_input, &NewDS);
-		status = AOM_save_with_extensions(NewDS);
+		status = AOM_save_with_extensions(NewDS);			
 		
+		//Dataset create DOCX
+		status = TCTYPE_find_type("MSWordX", "Dataset", &DocxDStype);
+		status = TCTYPE_construct_create_input(DocxDStype, &DocxDScreate_input);
+		status = AOM_set_value_string(DocxDScreate_input, "object_name", ItemName);
+		status = TCTYPE_create_object(DocxDScreate_input, &DocxNewDS);
+		status = AOM_save_with_extensions(DocxNewDS);		
 
-		//get latest Rev
-		status = ITEM_ask_latest_rev(NewItem, &Item_rev);
-
-		// GRM relation
+		// GRM relation --pdf
 		status = GRM_find_relation_type("IMAN_specification", &relation_type);
 		status = GRM_create_relation(Item_rev, NewDS, relation_type, NULLTAG, &GRMrelation);
 		status = GRM_save_relation(GRMrelation);
 		status = AOM_save_with_extensions(NewDS);
-		status = AOM_save_with_extensions(NewItem);	
-		
+		status = AOM_save_with_extensions(NewItem);			
 
-		//import name reference
-		status = AOM_refresh(NewDS, TRUE); // CHECKOUT THE DATASET	
+		// GRM relation --docx
+		status = GRM_find_relation_type("IMAN_reference", &Docxrelation_type);
+		status = GRM_create_relation(Item_rev, DocxNewDS, Docxrelation_type, NULLTAG, &GRMrelationDocx);
+		status = GRM_save_relation(GRMrelationDocx);
+		status = AOM_save_with_extensions(DocxNewDS);
+		status = AOM_save_with_extensions(NewItem);
+
+		//import name reference pdf and docx
+		status = AOM_refresh(NewDS, TRUE); // CHECKOUT THE PDF DATASET	
+		status = AOM_refresh(DocxNewDS, TRUE); // CHECKOUT THE WORD DATASET	
 		status = AE_import_named_ref(NewDS, "PDF_Reference", DSpath, "", SS_BINARY); // giving double backward slash in must(\\), if not given file will not import in dataset
+		status = AE_import_named_ref(DocxNewDS, "word", DocxDSPath, "", SS_BINARY);
 		status = AOM_save_without_extensions(NewDS);
+		status = AOM_save_without_extensions(DocxNewDS);
 		status = AOM_refresh(NewDS, FALSE); // CHECKIN THE DATASET				
+		status = AOM_refresh(DocxNewDS, FALSE); // CHECKin THE PDF DATASET
+
 
 		// place item to home folder		
 		status = FL_insert(NewFolder, NewItem, 999);
